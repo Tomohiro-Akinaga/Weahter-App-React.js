513a1d5a99f86a08aa1f796f24315ad1639fe0f9
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2022-02-15 16:59:20 -0800

    finished display today date

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,18 +1,14 @@
 import SearchBarStyle from "../SearchBar/SearchBar.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import AutocompleteView from "./AutocompleteView.jsx";
 
 export default function SearchBar() {
 
-    /* Controller role of MVC */
-    function handleChange() {
-        console.log("hello");
-    }
-
     return (
         <form className={SearchBarStyle.form} autoComplete="off">
-            <input className={SearchBarStyle.input} type="text" placeholder="Enter city name" onChange={handleChange}></input>
-            <button className={SearchBarStyle.button} type="button" onChange={handleChange}>
+            <input id="input" className={SearchBarStyle.input} type="text" placeholder="Enter city name" onChange={AutocompleteView}></input>
+            <button className={SearchBarStyle.button} type="button">
                 <FontAwesomeIcon className={SearchBarStyle.icon} icon={faMagnifyingGlass} />
             </button>
         </form>
