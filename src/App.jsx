b51632f1a1c7ac60cd373ac7c4b32d113932d9e1
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2022-02-18 00:12:03 -0800

    deleted script file from index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import React from "react";
 import SearchBar from "./components/SearchBar/SearchBar.jsx";
 import DisplayDate from "./components/DisplayDate/DisplayDate.jsx";
-// import CurrentWeather from "./components/CurrentWeather/CurrentWeather.jsx";
 import "./App.css";
 
 export default function App() {
