b2758f9051076f7f015277f2475405074ddc2f34
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2022-02-22 17:59:08 -0800

    modifying autocomolete jsx

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,31 @@
 import SearchBarStyle from "../SearchBar/SearchBar.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-
+import JSON from "./countryState.json";
+import Autocomplete from "./Autocomplete.jsx";
 
 export default function SearchBar() {
+    
+    function handleChange(event) {
+        const inputValue = event.target.value;
+        const normalizedKeyword = inputValue.trim().toLowerCase();
+        if(!normalizedKeyword) {
+            return;
+        }
+        const matchedName = JSON.data.filter(x => 
+            x.name.toLowerCase().substr(0, normalizedKeyword.length) === normalizedKeyword);
+            
+    }
+
     return (
         <div className={SearchBarStyle.container}>
             <form className={SearchBarStyle.form} autoComplete="off" type="submit">
-                <input className={SearchBarStyle.input} type="text" placeholder="Enter country name"></input>
+                <input className={SearchBarStyle.input} onChange={handleChange} type="text" placeholder="Enter country name"></input>
                 <button className={SearchBarStyle.button} type="submit">
                     <FontAwesomeIcon className={SearchBarStyle.icon} icon={faMagnifyingGlass} />
                 </button>
             </form>
+            <Autocomplete keyword={matchedName}/>
         </div>
     )
 }
