747fcd81aaad5f752137a906c8dbefeab3b97398
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2022-02-23 19:38:48 -0800

    start creating API call

--- src/components/SearchBar/Autocomplete.jsx
+++ src/components/SearchBar/Autocomplete.jsx
@@ -5,12 +5,11 @@ export default function Autocomplete(props) {
     
     const normalizedKeyword = props.keyword.trim().toLowerCase();
     const suggestedItem = JSON.data.filter(x => x.name.substr(0, normalizedKeyword.length).toLowerCase() === normalizedKeyword);
-    console.log(suggestedItem);
 
     return (
         <div className={SearchBarStyle.autocomplete}>
-            <ul className={SearchBarStyle.ul}>
-                {suggestedItem.map(item => <li className={SearchBarStyle.li}>{item.name}</li> )}
+            <ul className={SearchBarStyle.ul} >
+                {suggestedItem.map((item, index) => <li key={index} className={SearchBarStyle.li} onClick={props.onClick} >{item.name}</li> )}
             </ul>
         </div>
     )
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -7,6 +7,7 @@ import Autocomplete from "./Autocomplete.jsx";
 export default function SearchBar() {
 
     const [keyword, setKeyword] = React.useState("[]");
+    const inputRef = React.useRef(null);
 
     function handleChange(event) {
         if (!event.target.value) {
@@ -15,16 +16,25 @@ export default function SearchBar() {
             setKeyword(event.target.value);
         }
     }
-    
+
+    function handleClick(event) {
+        inputRef.current.value = event.target.innerText;
+    }   
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(event.target.querySelector(".SearchBar_input__V8-ey").value);
+    }
+
     return (
-        <div className={SearchBarStyle.container}>
-            <form className={SearchBarStyle.form} autoComplete="off" type="submit">
-                <input className={SearchBarStyle.input} onChange={handleChange} type="text" placeholder="Enter country name"></input>
+        <div className={SearchBarStyle.container} >
+            <form className={SearchBarStyle.form} autoComplete="off" type="submit" onSubmit={handleSubmit}>
+                <input className={SearchBarStyle.input} onChange={handleChange} type="text" placeholder="Enter country name" ref={inputRef}></input>
                 <button className={SearchBarStyle.button} type="submit">
                     <FontAwesomeIcon className={SearchBarStyle.icon} icon={faMagnifyingGlass} />
                 </button>
             </form>
-            <Autocomplete keyword={keyword}/>
+            <Autocomplete keyword={keyword} onClick={handleClick}/>
         </div>
     )
 }
