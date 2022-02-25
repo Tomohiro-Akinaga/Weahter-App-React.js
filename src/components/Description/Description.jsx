c35df0e5713542986897dc701f6b3aa98ce8e9a3
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2022-02-24 17:33:17 -0800

    finished creating middle container component

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,17 +1,23 @@
 import DescriptionStyle from "./Description.module.scss";
 
 export default function Description() {
-    const data = {
-        High: 23,
-        Wind: "7mph",
-    };
+    const data = [
+        { title: "High", text: "23°" },
+        { title: "Wind", text: "7mph" },
+        { title: "Sunrise", text: "05:27" },
+        { title: "Low", text: "14°" },
+        { title: "Rain", text: "0%" },
+        { title: "Sunset", text: "20:57" },
+    ];
     return (
         <div className={DescriptionStyle.container}>
             <ul className={DescriptionStyle.ul}>
-                <li className={DescriptionStyle.li}>
-                    <h2 className={DescriptionStyle.h2}>--</h2>
-                    <p className={DescriptionStyle.p}>--</p>
-                </li>
+                {data.map((item, index) => (
+                    <li key={index} className={DescriptionStyle.li}>
+                        <h2 className={DescriptionStyle.h2}>{item.text}</h2>
+                        <p className={DescriptionStyle.p}>{item.title}</p>
+                    </li>
+                ))}
             </ul>
         </div>
     );
