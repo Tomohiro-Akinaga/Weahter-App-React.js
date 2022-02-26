597d9a69056b5a860c9dbb21c511eced3304010e
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2022-02-25 21:26:45 -0800

    sucess fetch api only one time

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@ import SearchBar from "./components/SearchBar/SearchBar.jsx";
 import DisplayDate from "./components/DisplayDate/DisplayDate.jsx";
 import CurrentWeather from "./components/CurrentWeather/CurrentWeather.jsx";
 import Description from "./components/Description/Description.jsx";
-import SwitchBar from "./components/SwitchBar/SwitchBar.jsx";
 import HourlyItem from "./components/HourlyItem/HourlyItem.jsx";
 import "./App.scss";
 import { useEffect } from "react/cjs/react.development";
 
 export default function App() {
-    const [country, setCountry] = React.useState("Vancouver, Canada");
-    const [json, setJson] = React.useState("");
+    const [country, setCountry] = React.useState("Vancouver");
+    const [json, setJson] = React.useState();
 
     function handleSubmit(event) {
         event.preventDefault();
         setCountry(event.target.querySelector(".SearchBar_input__V8-ey").value);
     }
+
     useEffect(() => {
         fetch(
             `https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=7d20d69e5d5abc8385c9ae6416019816`
@@ -33,7 +33,7 @@ export default function App() {
                 <SearchBar onSubmit={handleSubmit} />
             </div>
             <div className="app-middle">
-                <CurrentWeather />
+                {json && <CurrentWeather json={json} />}
                 <Description />
             </div>
             <div className="app-bottom">
--- src/components/CurrentWeather/CurrentWeather.jsx
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -1,10 +1,11 @@
 // import CurrentWeatherImg from "http://openweathermap.org/img/wn/10d@2x.png";
 import CurrentWeatherStyle from "./CurrentWeather.module.scss";
 
-export default function CurrentWeather() {
+export default function CurrentWeather(props) {
+    console.log(props.json);
     return (
         <div className={CurrentWeatherStyle.container}>
-            <img className={CurrentWeatherStyle.img} src="http://openweathermap.org/img/wn/10d@2x.png"></img>
+            <img className={CurrentWeatherStyle.img}></img>
             <div className={CurrentWeatherStyle.text}>
                 <h2 className={CurrentWeatherStyle.degree}>21&deg;</h2>
                 <p className={CurrentWeatherStyle.climate}>Mostly Sunny</p>
@@ -12,3 +13,5 @@ export default function CurrentWeather() {
         </div>
     )
 };
+
+// src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
