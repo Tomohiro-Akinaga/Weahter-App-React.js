a389fd8d36b19eefaa4ce638369b388b91957aee
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2022-03-04 15:35:06 -0800

    done display date modify

--- src/components/DisplayDate/DisplayDate.jsx
+++ src/components/DisplayDate/DisplayDate.jsx
@@ -1,46 +1,52 @@
-import React from "react";
 import DisplayDateStyle from "./DisplayDate.module.css";
 
-export default function DisplayDate(props) {
-    const propsObject = { name:props.current.name, country:props.current.sys.country }
-    /* display today date */
-    const today = new Date();
-    const date = today.getDate();
-    function dayOfWeek() {
-        const dayIndex = today.getDay();
-        return [
-            "Sunday",
-            "Monday",
-            "Tuesday",
-            "Wedensday",
-            "Thursday",
-            "Friday",
-            "Saturday",
-        ][dayIndex];
-    }
-    function monthOfYear() {
-        const monthIndex = today.getMonth();
-        return [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December",
-        ][monthIndex];
-    }
-    const day = dayOfWeek();
-    const month = monthOfYear();
+const today = new Date();
+const date = today.getDate();
 
+function dayOfWeek() {
+    const dayIndex = today.getDay();
+    return [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wedensday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ][dayIndex];
+}
+
+function monthOfYear() {
+    const monthIndex = today.getMonth();
+    return [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ][monthIndex];
+}
+
+const day = dayOfWeek();
+const month = monthOfYear();
+
+export default function DisplayDate(props) {
+    const propsObject = {
+        name: props.current.name,
+        country: props.current.sys.country,
+    };
     return (
         <div className={DisplayDateStyle.container}>
-            <p className={DisplayDateStyle.city}>{propsObject.name}, {propsObject.country} </p>
+            <p className={DisplayDateStyle.city}>
+                {propsObject.name}, {propsObject.country}
+            </p>
             <p className={DisplayDateStyle.date}>
                 {month} {date}, {day}
             </p>
